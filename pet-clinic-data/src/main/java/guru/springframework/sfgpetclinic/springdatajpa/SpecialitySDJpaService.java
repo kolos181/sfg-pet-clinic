1467135dc327b818af792f86f95502b2bb13d9dc
Kolos182 <kolos181@gmail.com>
2020-12-17 18:38:46 +0200

    Implemented PetSDJpaService, SpecialitySDJpaService, VisitSDJpaService and created VisitService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,46 @@
+package guru.springframework.sfgpetclinic.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialityRepository;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityService) {
+        this.specialityRepository = specialityService;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
